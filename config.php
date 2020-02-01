688baf64895db5173ccc35043415d12745f6ba59
muslehm <55929657+muslehm@users.noreply.github.com>
2020-02-01 08:30:40 +0100

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ class myDB
     private $mysqli;
     public function __construct()
     {
-        $this->mysqli = new mysqli('sql205.epizy.com', 'epiz_24562454', 'Yef07FoFA2O', 'epiz_24562454_pd_db');
+        $this->mysqli = new mysqli('', '', '', '');
 
     }
     public function query($sql)
