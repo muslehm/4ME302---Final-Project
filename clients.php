6bb59eb6385c9fc57bdca54a4c552472cbda2c9f
muslehm <55929657+muslehm@users.noreply.github.com>
2020-02-01 08:32:54 +0100

    Update clients.php

--- clients.php
+++ clients.php
@@ -1,12 +1,12 @@
 <?php
 $gitClientArray = array(
-    'client_id' => '55281c9e567b8fcd1ec7',
-    'client_secret' => '24a879cc9435599744dbdbb5bdaeaad2e3f10761',
+    'client_id' => '',
+    'client_secret' => '',
     'redirect_uri' => 'http://musleh.epizy.com/github/login.php',
 );
 $liClientArray = array(
-    'client_id' => '777uh0t9phwkri',
-    'client_secret' => 'wJvhT3FP5Ely57eI',
+    'client_id' => '',
+    'client_secret' => '',
     'redirect_uri' => 'http://musleh.epizy.com/linkedin/login.php',
     'scope' => 'r_liteprofile r_emailaddress',
   
